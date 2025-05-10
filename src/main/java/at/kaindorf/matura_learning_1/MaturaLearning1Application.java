6dd4840dba5ab215f766649f49fa258a61aa2501
hocluc20 <hocluc20@htl-kaindorf.at>
2025-05-10 12:08:18 +0200

    init

---
+++ src/main/java/at/kaindorf/matura_learning_1/MaturaLearning1Application.java
@@ -0,0 +1,13 @@
+package at.kaindorf.matura_learning_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaturaLearning1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaturaLearning1Application.class, args);
+    }
+
+}
---
+++ src/test/java/at/kaindorf/matura_learning_1/MaturaLearning1ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.matura_learning_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaturaLearning1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
